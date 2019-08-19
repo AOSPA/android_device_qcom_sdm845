890799e4307a22aed1a7614f8ba3a136c7fc38f7
Jake Weinstein <jake@aospa.co>
2019-08-20 01:51:02 +0200

    sdm845: Build disabled vbmeta image
    Change-Id: Iaca48a734793ec0108aeedf5de46bb8fdec964f5

--- sdm845.mk
+++ sdm845.mk
@@ -7,6 +7,8 @@
 #BOARD_AVB_SYSTEM_ROLLBACK_INDEX := 0
 #BOARD_AVB_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
+BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+
 TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, device/qcom/common/common64.mk)
 #Inherit all except heap growth limit from phone-xhdpi-2048-dalvik-heap.mk
